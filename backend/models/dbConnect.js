78b04e3313b7ebe23c7c6735da3e262793123653
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-05-05 22:45:03 +0100

    Update dbConnect.js

--- backend/models/dbConnect.js
+++ backend/models/dbConnect.js
@@ -4,11 +4,11 @@ require('dotenv/config.js');
 
 const dbConnect = () => {
   mongoose.connect(
-    'mongodb+srv://developer_admin:access123@cluster0.w25j6.mongodb.net/logistics-planner?retryWrites=true&w=majority',
+    process.env.DB_CONNECTION_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
     console.log('Connected to DB :)')
   );
 }
 
-// process.env.DB_CONNECTION_URI
+
 module.exports = dbConnect;
